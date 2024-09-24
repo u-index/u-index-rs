71a2f2a70ef6d6821d30ed5f8dba42c857f40a96
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2024-09-24 17:06:00 +0200

    Add query benchmark

---
+++ src/bench.rs
@@ -0,0 +1,30 @@
+use std::hint::black_box;
+
+use super::*;
+
+impl UIndex {
+    /// Take `count` random substrings with length `len` and time querying them.
+    pub fn gen_query_positions(&self, len: usize, count: usize) -> Vec<(usize, usize)> {
+        let n = self.seq.len();
+
+        (0..count)
+            .map(|_| {
+                let pos = rand::random::<usize>() % (n - len);
+                (pos, pos + len)
+            })
+            .collect::<Vec<_>>()
+    }
+
+    /// Take `count` random substrings with length `len` and time querying them.
+    pub fn bench_positive(&self, queries: &[(usize, usize)]) -> f64 {
+        let start = std::time::Instant::now();
+
+        let mut num_matches = 0;
+        for &(s, e) in queries {
+            num_matches += self.query(&self.seq[s..e]).unwrap().count();
+        }
+        black_box(num_matches);
+
+        start.elapsed().as_secs_f64()
+    }
+}
