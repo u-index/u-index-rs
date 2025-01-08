0138db5df160946fea3ff79ddc37421897cfde7d
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2025-01-08 16:01:12 +0100

    update bench.rs to be &dyn

--- examples/bench.rs
+++ examples/bench.rs
@@ -1,10 +1,9 @@
 use packed_seq::{PackedSeqVec, SeqVec};
+use sdsl_lite_fm::*;
 use uindex::{
     bench::gen_query_positions,
-    indices::{
-        DivSufSortSa, FmAwryParams, FmBioParams, FmSdslIntParams, IndexBuilderEnum, LibSaisSa,
-    },
-    sketchers::{MinimizerParams, SketcherBuilderEnum},
+    indices::{FmAwryParams, FmBioParams, FmSdslParams, LibSaisSa},
+    sketchers::MinimizerParams,
     utils::{read_chromosomes, Timer},
     IndexBuilder, UIndex,
 };
@@ -14,82 +13,41 @@ fn main() {
 
     let seq: PackedSeqVec = read_chromosomes(1);
 
-    let sketch_params = SketcherBuilderEnum::Minimizer(MinimizerParams {
+    let sketch_params = Box::new(MinimizerParams {
         k: 28,
         l: 128,
         remap: true,
         cacheline_ef: true,
+        skip_zero: false,
     });
 
     let queries = gen_query_positions(seq.as_slice(), 256, 10000);
 
-    // Fm Sdsl
-    if true {
-        let index_params = IndexBuilderEnum::FmSdsl(FmSdslIntParams);
-
-        let u = UIndex::build(seq.clone(), sketch_params, index_params);
-
-        let _t = Timer::new("bench_positive");
-        u.bench_positive(&queries);
-    }
-
-    // Fm Awry
-    if true {
-        let index_params = IndexBuilderEnum::FmAwry(FmAwryParams { sa_sampling: 64 });
-
-        let u = UIndex::build(seq.clone(), sketch_params, index_params);
-
-        let _t = Timer::new("bench_positive");
-        u.bench_positive(&queries);
-    }
-
-    // SAIS
-    if true {
-        let index_params = IndexBuilderEnum::LibSaisSa(LibSaisSa {
+    let index_params: Vec<Box<dyn IndexBuilder>> = vec![
+        Box::new(FmSdslParams::<FmIndexByte32Ptr, _>::new()),
+        Box::new(FmSdslParams::<FmIndexInt32Ptr, _>::new()),
+        Box::new(FmSdslParams::<FmIndexByte64Ptr, _>::new()),
+        Box::new(FmSdslParams::<FmIndexInt64Ptr, _>::new()),
+        Box::new(FmAwryParams { sa_sampling: 64 }),
+        Box::new(LibSaisSa {
             store_ms_seq: true,
             par: false,
-        });
-
-        let u = UIndex::build(seq.clone(), sketch_params, index_params);
-
-        let _t = Timer::new("bench_positive");
-        u.bench_positive(&queries);
-    }
-
-    // Fm Bio
-    if true {
-        let index_params = IndexBuilderEnum::FmBio(FmBioParams {
+        }),
+        Box::new(FmBioParams {
             occ_sampling: 64,
             sa_sampling: 64,
-        });
-
-        let u = UIndex::build(seq.clone(), sketch_params, index_params);
-
-        let _t = Timer::new("bench_positive");
-        u.bench_positive(&queries);
-    }
-
-    // SAIS parallel
-    if false {
-        let index_params = IndexBuilderEnum::LibSaisSa(LibSaisSa {
-            store_ms_seq: true,
-            par: true,
-        });
-
-        let u = UIndex::build(seq.clone(), sketch_params, index_params);
-
-        let _t = Timer::new("bench_positive");
-        u.bench_positive(&queries);
-    }
-
-    // DivSufSort
-    if false {
-        let index_params = IndexBuilderEnum::DivSufSortSa(DivSufSortSa {
-            store_ms_seq: true,
-            compress: true,
-        });
-
-        let u = UIndex::build(seq.clone(), sketch_params, index_params);
+        }),
+        // Box::new(LibSaisSa {
+        //     store_ms_seq: true,
+        //     par: true,
+        // }),
+        // Box::new(DivSufSortSa {
+        //     store_ms_seq: true,
+        //     compress: true,
+        // }),
+    ];
+    for p in index_params {
+        let u = UIndex::build(seq.clone(), sketch_params.clone(), p);
 
         let _t = Timer::new("bench_positive");
         u.bench_positive(&queries);
