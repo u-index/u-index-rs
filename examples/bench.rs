50ba1000bad48271389edad0f56c04966a73b9b0
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2025-01-08 15:20:52 +0100

    Update examples/bench.rs for Awry and Sdsl

--- examples/bench.rs
+++ examples/bench.rs
@@ -1,23 +1,47 @@
 use packed_seq::{PackedSeqVec, SeqVec};
 use uindex::{
     bench::gen_query_positions,
-    indices::{DivSufSortSa, FmAwryParams, FmBioParams, IndexBuilderEnum, LibSaisSa},
+    indices::{
+        DivSufSortSa, FmAwryParams, FmBioParams, FmSdslIntParams, IndexBuilderEnum, LibSaisSa,
+    },
     sketchers::{MinimizerParams, SketcherBuilderEnum},
     utils::{read_chromosomes, Timer},
-    UIndex,
+    IndexBuilder, UIndex,
 };
 
 fn main() {
+    color_backtrace::install();
+
     let seq: PackedSeqVec = read_chromosomes(1);
 
     let sketch_params = SketcherBuilderEnum::Minimizer(MinimizerParams {
-        k: 8,
-        l: 16,
-        remap: false,
-        cacheline_ef: false,
+        k: 28,
+        l: 128,
+        remap: true,
+        cacheline_ef: true,
     });
 
-    let mut queries = vec![];
+    let queries = gen_query_positions(seq.as_slice(), 256, 10000);
+
+    // Fm Sdsl
+    if true {
+        let index_params = IndexBuilderEnum::FmSdsl(FmSdslIntParams);
+
+        let u = UIndex::build(seq.clone(), sketch_params, index_params);
+
+        let _t = Timer::new("bench_positive");
+        u.bench_positive(&queries);
+    }
+
+    // Fm Awry
+    if true {
+        let index_params = IndexBuilderEnum::FmAwry(FmAwryParams { sa_sampling: 64 });
+
+        let u = UIndex::build(seq.clone(), sketch_params, index_params);
+
+        let _t = Timer::new("bench_positive");
+        u.bench_positive(&queries);
+    }
 
     // SAIS
     if true {
@@ -27,8 +51,6 @@ fn main() {
         });
 
         let u = UIndex::build(seq.clone(), sketch_params, index_params);
-
-        queries = gen_query_positions(seq.as_slice(), 256, 1000000);
 
         let _t = Timer::new("bench_positive");
         u.bench_positive(&queries);
@@ -43,20 +65,6 @@ fn main() {
 
         let u = UIndex::build(seq.clone(), sketch_params, index_params);
 
-        // queries = u.gen_query_positions(256, 1000000);
-
-        let _t = Timer::new("bench_positive");
-        u.bench_positive(&queries);
-    }
-
-    // Fm Awry
-    if true {
-        let index_params = IndexBuilderEnum::FmAwry(FmAwryParams { sa_sampling: 64 });
-
-        let u = UIndex::build(seq.clone(), sketch_params, index_params);
-
-        // queries = u.gen_query_positions(256, 1000000);
-
         let _t = Timer::new("bench_positive");
         u.bench_positive(&queries);
     }
@@ -70,8 +78,6 @@ fn main() {
 
         let u = UIndex::build(seq.clone(), sketch_params, index_params);
 
-        // queries = u.gen_query_positions(256, 1000000);
-
         let _t = Timer::new("bench_positive");
         u.bench_positive(&queries);
     }
@@ -84,8 +90,6 @@ fn main() {
         });
 
         let u = UIndex::build(seq.clone(), sketch_params, index_params);
-
-        // let queries = u.gen_query_positions(256, 1000000);
 
         let _t = Timer::new("bench_positive");
         u.bench_positive(&queries);
