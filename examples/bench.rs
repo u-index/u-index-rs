e3668ce9d32e88a9cea309ea6ca1500bbd0dccd2
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2025-01-23 00:26:22 +0100

    Run experiments in subprocess

--- examples/bench.rs
+++ examples/bench.rs
@@ -1,9 +1,12 @@
+#![feature(lazy_get)]
 #![allow(unused)]
-use std::{any::type_name, collections::HashMap};
+use std::{any::type_name, cell::Cell, collections::HashMap, sync::LazyLock};
 
+use clap::Parser;
 use packed_seq::{AsciiSeqVec, PackedSeqVec, SeqVec};
 use sdsl_lite_fm::*;
 use serde_json::{Number, Value};
+use tracing::info;
 use uindex::{
     bench::gen_query_positions,
     indices::{DivSufSortSa, FmAwryParams, FmSdslParams, LibSaisSa},
@@ -13,11 +16,26 @@ use uindex::{
     IndexBuilder, SketcherBuilder, UIndex,
 };
 
+#[derive(Parser)]
+struct Args {
+    /// Run experiments in subprocesses.
+    #[clap(long)]
+    ext: bool,
+    #[clap(long)]
+    run_id: Option<usize>,
+}
+
+static ARGS: LazyLock<Args> = LazyLock::new(|| Args::parse());
+
+thread_local! {
+    static ID: Cell<usize> = Cell::new(0);
+}
+
 fn main() {
     color_backtrace::install();
     *INIT_TRACE;
 
-    {
+    if ARGS.run_id.is_none_or(|run_id| run_id < 1000) {
         let (seq, _ranges) = read_chromosomes::<PackedSeqVec>(1);
 
         let query_length = 512;
@@ -34,10 +52,16 @@ fn main() {
         );
 
         // Write all_stats.
-        let stats_string = serde_json::to_string(&all_stats).unwrap();
-        std::fs::write("stats.json", stats_string).unwrap();
+        if ARGS.run_id.is_none() {
+            let stats_string = serde_json::to_string(&all_stats).unwrap();
+            std::fs::write("stats.json", stats_string).unwrap();
+        }
     }
 
+    ID.set(1000);
+    if ARGS
+        .run_id
+        .is_none_or(|run_id| 1000 <= run_id && run_id < 2000)
     {
         let seq = std::fs::read("english.200MB").unwrap();
 
@@ -55,10 +79,16 @@ fn main() {
         );
 
         // Write all_stats.
-        let stats_string = serde_json::to_string(&all_stats).unwrap();
-        std::fs::write("stats-english.json", stats_string).unwrap();
+        if ARGS.run_id.is_none() {
+            let stats_string = serde_json::to_string(&all_stats).unwrap();
+            std::fs::write("stats-english.json", stats_string).unwrap();
+        }
     }
 
+    ID.set(2000);
+    if ARGS
+        .run_id
+        .is_none_or(|run_id| 2000 <= run_id && run_id < 3000)
     {
         let seq = std::fs::read("proteins.200MB").unwrap();
 
@@ -76,8 +106,14 @@ fn main() {
         );
 
         // Write all_stats.
-        let stats_string = serde_json::to_string(&all_stats).unwrap();
-        std::fs::write("stats-proteins.json", stats_string).unwrap();
+        if ARGS.run_id.is_none() {
+            let stats_string = serde_json::to_string(&all_stats).unwrap();
+            std::fs::write("stats-proteins.json", stats_string).unwrap();
+        }
+    }
+
+    if let Some(run_id) = ARGS.run_id {
+        panic!("Did non find run id {run_id}");
     }
 }
 
@@ -163,8 +199,10 @@ fn run<SV: SeqVec>(
         };
 
         for (p, s) in params {
-            tracing::warn!("Building UIndex with params {:?} {:?}", &*s, &*p);
-            if let Some(u) = UIndex::<SV>::try_build_with_ranges(seq.clone(), &ranges, &*s, &*p) {
+            let stats = run_fn(|| {
+                tracing::warn!("Building UIndex with params {:?} {:?}", &*s, &*p);
+
+                let u = UIndex::<SV>::try_build_with_ranges(seq.clone(), &ranges, &*s, &*p)?;
                 let query_time = {
                     let _t = Timer::new("bench_positive").info();
                     u.bench_positive(&queries)
@@ -177,6 +215,9 @@ fn run<SV: SeqVec>(
                     "query_time",
                     Value::Number(Number::from_f64(query_time).unwrap()),
                 );
+                Some(stats)
+            });
+            if let Some(stats) = stats {
                 all_stats.push(stats);
             }
         }
@@ -184,51 +225,105 @@ fn run<SV: SeqVec>(
         // SIndex without sketching
 
         if k == 0 {
-            tracing::warn!("Building plain-text SIndex");
-            let u = SIndex::build(seq.clone(), 1, 1);
+            let stats = run_fn(|| {
+                tracing::warn!("Building plain-text SIndex");
+                let u = SIndex::build(seq.clone(), 1, 1);
 
-            let query_time = {
-                let _t = Timer::new("bench_positive").info();
-                u.bench_positive(&queries)
-            };
-            let mut stats = u.stats();
-            stats.insert("index", Value::String("sparse SA".to_string()));
-            stats.insert("index_params", Value::String("sparse SA".to_string()));
-            stats.insert(
-                "sketch_params",
-                Value::String(format!("SIndex {{ k: {k}, l: {l} }}")),
-            );
-            stats.insert("sketch_l", Value::Number(Number::from(l)));
-            stats.insert("sketch_k", Value::Number(Number::from(k)));
-            stats.insert("query_length", Value::Number(Number::from(query_length)));
-            stats.insert(
-                "query_time",
-                Value::Number(Number::from_f64(query_time).unwrap()),
-            );
-            all_stats.push(stats);
+                let query_time = {
+                    let _t = Timer::new("bench_positive").info();
+                    u.bench_positive(&queries)
+                };
+                let mut stats = u.stats();
+                stats.insert("index", Value::String("sparse SA".to_string()));
+                stats.insert("index_params", Value::String("sparse SA".to_string()));
+                stats.insert(
+                    "sketch_params",
+                    Value::String(format!("SIndex {{ k: {k}, l: {l} }}")),
+                );
+                stats.insert("sketch_l", Value::Number(Number::from(l)));
+                stats.insert("sketch_k", Value::Number(Number::from(k)));
+                stats.insert("query_length", Value::Number(Number::from(query_length)));
+                stats.insert(
+                    "query_time",
+                    Value::Number(Number::from_f64(query_time).unwrap()),
+                );
+                Some(stats)
+            });
+            if let Some(stats) = stats {
+                all_stats.push(stats);
+            }
         }
         if k > 0 {
-            tracing::warn!("Building minizer-space SIndex");
-            let u = SIndex::build(seq.clone(), k, l);
-            let query_time = {
-                let _t = Timer::new("bench_positive").info();
-                u.bench_positive(&queries)
-            };
-            let mut stats = u.stats();
-            stats.insert("index", Value::String("sparse SA".to_string()));
-            stats.insert("index_params", Value::String("SIndex".to_string()));
-            stats.insert(
-                "sketch_params",
-                Value::String(format!("SIndex {{ k: {k}, l: {l} }}")),
-            );
-            stats.insert("sketch_l", Value::Number(Number::from(l)));
-            stats.insert("sketch_k", Value::Number(Number::from(k)));
-            stats.insert("query_length", Value::Number(Number::from(query_length)));
-            stats.insert(
-                "query_time",
-                Value::Number(Number::from_f64(query_time).unwrap()),
-            );
-            all_stats.push(stats);
+            let stats = run_fn(|| {
+                tracing::warn!("Building minizer-space SIndex");
+                let u = SIndex::build(seq.clone(), k, l);
+                let query_time = {
+                    let _t = Timer::new("bench_positive").info();
+                    u.bench_positive(&queries)
+                };
+                let mut stats = u.stats();
+                stats.insert("index", Value::String("sparse SA".to_string()));
+                stats.insert("index_params", Value::String("SIndex".to_string()));
+                stats.insert(
+                    "sketch_params",
+                    Value::String(format!("SIndex {{ k: {k}, l: {l} }}")),
+                );
+                stats.insert("sketch_l", Value::Number(Number::from(l)));
+                stats.insert("sketch_k", Value::Number(Number::from(k)));
+                stats.insert("query_length", Value::Number(Number::from(query_length)));
+                stats.insert(
+                    "query_time",
+                    Value::Number(Number::from_f64(query_time).unwrap()),
+                );
+                Some(stats)
+            });
+            if let Some(stats) = stats {
+                all_stats.push(stats);
+            }
         }
+    }
+}
+
+fn run_fn(
+    f: impl Fn() -> Option<HashMap<&'static str, Value>>,
+) -> Option<HashMap<&'static str, Value>> {
+    if ARGS.ext {
+        ID.set(ID.get() + 1);
+        if let Some(run_id) = ARGS.run_id {
+            if run_id == ID.get() {
+                info!("RUNNING FOR ID {}", ID.get());
+                let mut result = f();
+                if let Some(result) = result.as_mut() {
+                    result.insert("id", Value::Number(Number::from(ID.get())));
+                }
+                println!("{}", serde_json::to_string(&result).unwrap());
+                info!("RUNNING FOR ID {} DONE", ID.get());
+                info!("OUTPUT {}", serde_json::to_string(&result).unwrap());
+                std::process::exit(0);
+            } else {
+                // This is not the run.
+                None
+            }
+        } else {
+            // Launch subprocess to run this experiment.
+            let program = std::env::current_exe().unwrap();
+            info!("LAUNCHING subprocess for ID {} {:?}", ID.get(), program);
+            let output = std::process::Command::new(&program)
+                .arg("--ext")
+                .arg("--run-id")
+                .arg(&format!("{}", ID.get()))
+                .stderr(std::process::Stdio::inherit())
+                .output()
+                .unwrap();
+            info!("READING   subprocess for ID {} {:?}", ID.get(), program);
+            // FIXME: Just leaking all the output so that we can get static strings to it.
+            let output = Box::new(output.stdout).leak();
+            // info!("READING   OUTPUT {}", std::str::from_utf8(output).unwrap());
+            let result = serde_json::from_slice(output).unwrap();
+            result
+        }
+    } else {
+        // Just run directly here.
+        f()
     }
 }
